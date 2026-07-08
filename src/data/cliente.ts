9fa688f6d10f3cb18d97a257ef27b7af411432d1
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-07 21:18:49 -0600

    feat: crea el arreglo de datos de Cliente

---
+++ src/data/cliente.ts
@@ -0,0 +1,13 @@
+import { Cliente } from '../models/cliente';
+
+export const clientes: Cliente[] = [
+    {
+        id: 1,
+        nombre: "",
+        apellido: "",
+        contrasena: "",
+        telefono: 0,
+        email: "",
+        tarjetaCredito: 0
+    }
+];
