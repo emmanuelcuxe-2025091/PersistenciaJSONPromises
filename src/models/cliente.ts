e6ce7b6c331f108d6dedef2e4497b0d7e2d55580
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-07 20:45:36 -0600

    feat: añade la interfaz de Cliente

---
+++ src/models/cliente.ts
@@ -0,0 +1,9 @@
+export interface Cliente {
+    id: number;
+    nombre: string;
+    apellido: string;
+    contrasena: string;
+    telefono: number;
+    email: string;
+    tarjetaCredito: number;
+};
