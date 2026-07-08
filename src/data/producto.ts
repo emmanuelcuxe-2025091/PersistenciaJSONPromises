26300f4f8e8eb409581a41633e3fdf7d85d91f13
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-07 21:12:39 -0600

    feat: crea el arreglo de datos de Producto

---
+++ src/data/producto.ts
@@ -0,0 +1,14 @@
+import { Producto } from '../models/producto';
+
+export const productos: Producto[] = [
+    {
+        id: 0,
+        nombre: "",
+        descripcion: "",
+        precio: 0,
+        stock: 0,
+        marca: "",
+        estado: "Activo",
+        categoria: "Otros"
+    }
+];
