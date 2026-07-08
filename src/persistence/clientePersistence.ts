3b8b3412041826ee92546d35fc198743f9684d2f
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-08 00:07:06 -0600

    feat: implementa un manejo de errores a las persistencias

--- src/persistence/clientePersistence.ts
+++ src/persistence/clientePersistence.ts
@@ -2,15 +2,23 @@ import { readFile, writeFile } from "fs/promises";
 import { Cliente } from "../models/cliente";
 
 export async function leerClientes(): Promise<Cliente[]> {
-    const data = await readFile("./src/data/cliente.json", "utf-8");
-    
-    return JSON.parse(data) as Cliente[];
+    try {
+        const data = await readFile("./src/data/cliente.json", "utf-8");
+        return JSON.parse(data) as Cliente[];
+    } catch (error) {
+        console.error("Error:", error);
+        return [];
+    }
 }
 
 export async function guardarClientes(clientes: Cliente[]): Promise<void> {
-    await writeFile(
-        "./src/data/cliente.json",
-        JSON.stringify(clientes, null, 2),
-        "utf-8"
-    );
+    try {
+        await writeFile(
+            "./src/data/cliente.json",
+            JSON.stringify(clientes, null, 2),
+            "utf-8"
+        );
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
--- src/persistence/productoPersistence.ts
+++ src/persistence/productoPersistence.ts
@@ -2,15 +2,23 @@ import { readFile, writeFile } from 'fs/promises';
 import { Producto } from '../models/producto';
 
 export async function leerProductos(): Promise<Producto[]> {
-    const data = await readFile("./src/data/producto.json", "utf-8");
-
-    return JSON.parse(data) as Producto[];
+    try {
+        const data = await readFile("./src/data/producto.json", "utf-8");
+        return JSON.parse(data) as Producto[];
+    } catch (error) {
+        console.error("Error:", error);
+        return [];
+    }
 }
 
 export async function guardarProductos(productos: Producto[]): Promise<void> {
-    await writeFile(
-        "./src/data/producto.json",
-        JSON.stringify(productos, null, 2),
-        "utf-8"
-    );
+    try {
+        await writeFile(
+            "./src/data/producto.json",
+            JSON.stringify(productos, null, 2),
+            "utf-8"
+        );
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
