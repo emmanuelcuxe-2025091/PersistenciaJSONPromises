d376531c3241cb3601272a7616f89ca1bc306f3a
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-10 23:16:28 -0600

    feat: agrega validaciones faltantes de Cliente

--- src/services/clienteService.ts
+++ src/services/clienteService.ts
@@ -1,7 +1,12 @@
-import { Cliente } from "../models/cliente";
-import { leerClientes, guardarClientes } from "../persistence/clientePersistence";
+import { Cliente } from '../models/cliente';
+import { leerClientes, guardarClientes } from '../persistence/clientePersistence';
 
 export function validarCliente(cliente: Cliente): boolean {
+
+    if (cliente.id <= 0) {
+        console.log("El ID debe ser mayor que 0.");
+        return false;
+    }
 
     if (cliente.nombre.trim() === "") {
         console.log("El nombre es obligatorio.");
@@ -13,9 +18,44 @@ export function validarCliente(cliente: Cliente): boolean {
         return false;
     }
 
-    if (cliente.email.trim() === "") {
-        console.log("El correo es obligatorio.");
+    if (cliente.contrasena.trim() === "") {
+        console.log("La contraseña es obligatoria.");
         return false;
+    }
+
+    if (cliente.contrasena.length < 8) {
+    console.log("La contraseña debe tener al menos 8 caracteres.");
+    return false;
+    }
+
+    if (cliente.telefono <= 0) {
+        console.log("El teléfono no es válido.");
+        return false;
+    }
+
+    if (cliente.telefono.toString().length !== 8) {
+    console.log("El teléfono debe tener 8 dígitos.");
+    return false;
+    }
+
+    if (cliente.email.trim() === "") {
+        console.log("El correo electrónico es obligatorio.");
+        return false;
+    }
+
+    if (!cliente.email.includes("@")) {
+        console.log("El correo electrónico no es válido.");
+        return false;
+    }
+
+    if (cliente.tarjetaCredito <= 0) {
+        console.log("La tarjeta de crédito no es válida.");
+        return false;
+    }
+
+    if (cliente.tarjetaCredito.toString().length !== 16) {
+    console.log("La tarjeta de crédito debe tener 16 dígitos.");
+    return false;
     }
 
     return true;
